0d7aeebcedc005bc8c3492f5f3bd75cd41ecbf8e
0xrishitripathi <131165109+0xrishitripathi@users.noreply.github.com>
2023-09-07 01:05:41 +0530

    Add files via upload
    Added initialization script

---
+++ avail-anywhere.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Step 1: Install necessary packages
+for pkg in wget tar proot-distro; do
+    if ! dpkg -s $pkg >/dev/null 2>&1; then
+        pkg install $pkg -y
+    fi
+done
+
+# Step 2: Install Ubuntu
+if [ ! -d "/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu" ]; then
+  proot-distro install ubuntu
+fi
+
+# Step 3: Login to the Ubuntu container and execute the commands
+proot-distro login ubuntu -- bash -c '\
+  wget https://github.com/availproject/avail-light/releases/download/v1.6.0-rc1/avail-light-linux-aarch64.tar.gz && \
+  tar -xf avail-light-linux-aarch64.tar.gz && \
+  wget https://raw.githubusercontent.com/availproject/availproject.github.io/main/static/kate/avail-light-1.4.3/config.yaml && \
+  ./avail-light-linux-aarch64 --config config.yaml'
